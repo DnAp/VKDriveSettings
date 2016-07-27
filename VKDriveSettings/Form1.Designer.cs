43c468b39c383209240d095300a4e939dec3430d
DnAp <dnlebedev@gmail.com>
2016-07-27 22:47:51 +0300

    first commit

---
+++ VKDriveSettings/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace VKDriveSettings
+{
+	internal partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelActionName = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // labelActionName
+            // 
+            this.labelActionName.AutoSize = true;
+            this.labelActionName.Location = new System.Drawing.Point(12, 9);
+            this.labelActionName.Name = "labelActionName";
+            this.labelActionName.Size = new System.Drawing.Size(87, 13);
+            this.labelActionName.TabIndex = 1;
+            this.labelActionName.Text = "labelActionName";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 58);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(484, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(376, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(397, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 80);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.labelActionName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ВКДрайв настройки: ";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelActionName;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ VKDriveSettings/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+using VKDriveSettings.Properties;
+
+namespace VKDriveSettings
+{
+	internal partial class Form1 : Form
+    {
+        const int TYPE_TEXT = 1;
+        string errorMessage = "Что-то пошло не так(";
+        string skipMessage = "Что-то пошло не так(";
+
+        public Form1(string[] args)
+        {
+
+            int type;
+            string name, actionName;
+
+			if (args.Length > 0)
+			{
+				switch (args[0])
+				{
+					case "--GC":
+						Directory.CreateDirectory("\\_SYSTEM\\GC");
+						MessageBox.Show(Resources.Form1_Form1_Операция_выполнена);
+						Environment.Exit(0);
+						break;
+				}
+			}
+
+			try
+            {
+                var document = new XmlDocument();
+                document.Load("VKDirvePathData.xml");
+
+                type = Convert.ToInt32(readElement(document, "type"));
+                name = readElement(document, "name");
+                actionName = readElement(document, "actionName");
+                errorMessage = readElement(document, "errorMessage");
+                skipMessage = readElement(document, "skipMessage");
+            }
+            catch (Exception) {
+                MessageBox.Show(Resources.Form1_Form1_nor_vkdrive_drive);
+                Environment.Exit(0);
+                return;
+            }
+
+	        InitializeComponent();
+
+            if (type == TYPE_TEXT)
+            {
+                Text += name;
+                labelActionName.Text = actionName;
+            }
+            
+
+            toolStripStatusLabel1.Text = AppDomain.CurrentDomain.BaseDirectory;
+        }
+        
+        string readElement(XmlDocument document, string name)
+        {
+            XmlNode node = document.GetElementsByTagName(name)[0];
+            return node.InnerText;
+        }
+
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] fileListOld = Directory.GetDirectories(Directory.GetCurrentDirectory());
+
+                string filename = Regex.Replace(textBox1.Text, @"http(s|)://.*/", "");
+
+                Directory.CreateDirectory(Directory.GetCurrentDirectory()+"\\"+filename);
+                string[] fileListNew = Directory.GetDirectories(Directory.GetCurrentDirectory());
+
+                if (fileListOld.Length == fileListNew.Length - 1)
+                {
+                    string diff = null;
+                    for (int i = 0; i < fileListNew.Length; i++)
+                    {
+                        if (fileListNew.Length - 1 == i)
+                        {
+                            diff = fileListNew[i];
+                            break;
+                        }
+						if (fileListOld[i] != fileListNew[i])
+                        {
+                            bool isNew = true;
+                            for (int j = 0; j < fileListOld.Length; j++)
+                            {
+                                if (fileListOld[j] == fileListNew[i])
+                                {
+                                    isNew = false;
+                                }
+                            }
+                            if (isNew)
+                            {
+                                diff = fileListNew[i];
+                                break;
+                            }
+                        }
+                    }
+                    MessageBox.Show("Теперь появился новый каталог: "+diff.Substring(Directory.GetCurrentDirectory().Length+1));
+                }
+                else
+                {
+                    MessageBox.Show(skipMessage, "Что-то пошло не так");
+                }
+                Close();
+                
+            }catch(Exception){
+                MessageBox.Show(errorMessage, "Что-то пошло не так");
+            }
+        }
+
+    }
+}
